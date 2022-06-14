1b5b712f100aa23d23489818b77f961651390e8e
Smert <rozanov7475@gmail.com>
2022-06-14 17:51:31 +0300

    1

--- bot.py
+++ bot.py
@@ -5,130 +5,85 @@ import random
 
 t = 0
 k = 300
-listx = [850, 1067, 1187]
+list1buttons = {'850': '970', '1067': '951', '1187': '950'}
+positions = {'987': '600', '947': '601', '975': '800', '936': '809', '946': '513', '972': '513', '940': '808',
+             '1117': '575', '804': '578'}
+
+def transformation():
+    a = str(pag.position())
+    b = a.replace('Point', '')
+    elements = b[1:-1].split(",")
+    mytuple = tuple(elements)
+    x2 = mytuple[0]
+    y2 = mytuple[1].replace(' ', '')
+    x2 = int(x2.replace('x=', ''))
+    y2 = int(y2.replace('y=', ''))
+    print(f'{x2}, {y2}')
+    print(pag.pixel(x2, y2))
+    time.sleep(0.15)
+    kb.release("i")
+    # преоброзование Point(x,y) в x и y
+
+def click1(x, y):
+    pag.mouseDown(x, y, button='left')
+    pag.mouseUp(x, y, button='left')
+
+def exit():
+    click1(x=952, y=947)
+    time.sleep(8)
+
+def start():
+    time.sleep(2.5)
+    click1(x=1076, y=703)
+    time.sleep(0.2)
+    # кнопка1(диско)
+    click1(x=1105, y=757)
+    time.sleep(0.2)
+    # кнопка2(режим)
+    click1(x=957, y=680)
+    time.sleep(0.1)
+    # сундук
+    time.sleep(9)
+    # ожидание
+
+def play():
+    start()
+    for j in range(1, 100):
+        if pag.pixel(944, 948) == (255, 255, 255) and pag.pixel(966, 946) == (255, 255, 255) \
+                and pag.pixel(1005, 944) == (104, 187, 255) and pag.pixel(923, 946) == (104, 187, 255):
+            exit()
+            break
+        #проверка на кнопку
+        l1 = []
+        k1 = random.choice(tuple(list1buttons.items()))
+        for x in list(k1):
+            a = x
+            l1.append(a)
+        x1 = l1[0]
+        y1 = l1[1]
+
+        l2 = []
+        k2 = random.choice(tuple(positions.items()))
+        for x in list(k2):
+            a1 = x
+            l2.append(a1)
+        x3 = l2[0]
+        y3 = l2[1]
+        click1(int(x1), int(y1))
+        time.sleep(random.randint(40, 50)/100)
+        click1(int(x3), int(y3))
+        time.sleep(random.randint(330, 360)/100)
 
 while True:
     if kb.is_pressed("p"):
+        kb.release("p")
         for i in range(1, k):
-            time.sleep(2.5)
-            pag.mouseDown(x=1076, y=703, button='left')
-            pag.mouseUp(x=1076, y=703, button='left')
-            time.sleep(0.2)
-            #кнопка1(диско)
-
-            pag.mouseDown(x=1105, y=757, button='left')
-            pag.mouseUp(x=1105, y=757, button='left')
-            time.sleep(0.2)
-            #кнопка2(режим)
-
-            pag.mouseDown(x=957, y=680, button='left')
-            pag.mouseUp(x=957, y=680, button='left')
-            time.sleep(0.1)
-            #сундук
-
-            time.sleep(9)
-            #ожидание
-
-            for j in range(1, 43):
-                d = random.randrange(1, 11, 1)
-                x1 = random.choice(listx)
-                if x1 == 850:
-                    y1 = 970
-                elif x1 == 1067:
-                    y1 = 951
-                elif x1 == 1187:
-                    y1 = 950
-                if d == 1 or d == 10:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.51)
-                    pag.mouseDown(x=987, y=600, button='left')
-                    pag.mouseUp(x=967, y=607, button='left')
-                    time.sleep(3.4)
-                    #1
-
-                elif d == 2 or d == 11:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.52)
-                    pag.mouseDown(x=947, y=601, button='left')
-                    pag.mouseUp(x=947, y=601, button='left')
-                    time.sleep(3.6)
-                    #2
-
-                elif d == 3:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.46)
-                    pag.mouseDown(x=975, y=800, button='left')
-                    pag.mouseUp(x=975, y=800, button='left')
-                    time.sleep(3.5)
-                    #3
-
-                elif d == 4:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.45)
-                    pag.mouseDown(x=936, y=809, button='left')
-                    pag.mouseUp(x=936, y=809, button='left')
-                    time.sleep(3.3)
-                    #4
-
-                elif d == 5:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.41)
-                    pag.mouseDown(x=946, y=513, button='left')
-                    pag.mouseUp(x=946, y=513, button='left')
-                    time.sleep(3.4)
-                    #5
-
-                elif d == 6:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.423)
-                    pag.mouseDown(x=972, y=513, button='left')
-                    pag.mouseUp(x=972, y=513, button='left')
-                    time.sleep(4.2)
-                    #6
-
-                elif d == 7:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.49)
-                    pag.mouseDown(x=940, y=808, button='left')
-                    pag.mouseUp(x=940, y=808, button='left')
-                    time.sleep(3.1)
-                    #7
-
-                elif d == 8:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.481)
-                    pag.mouseDown(x=1117, y=575, button='left')
-                    pag.mouseUp(x=1117, y=575, button='left')
-                    time.sleep(3.4)
-                    #8
-
-                elif d == 9:
-                    pag.mouseDown(x1, y1, button='left')
-                    pag.mouseUp(x1, y1, button='left')
-                    time.sleep(0.432)
-                    pag.mouseDown(x=804, y=578, button='left')
-                    pag.mouseUp(x=804, y=578, button='left')
-                    time.sleep(3.8)
-                    #9
-
-            #вариации ходов
-            t = t + 1
+            play()
+            t = t+1
             print(t)
-            pag.mouseDown(x = 952, y = 947, button='left')
-            pag.mouseUp(x = 952, y = 947, button='left')
-            time.sleep(8)
-            kb.release("p")
-            #выход
     elif kb.is_pressed("o"):
         print(pag.position())
         time.sleep(0.15)
         kb.release("o")
-        # берёт позиции
+    elif kb.is_pressed("i"):
+        transformation()
